fcab7a02e29f71d98dda70cfdb9f22d176b7f3b4
tpaglia493 <tommasopaglia93@outlook.it>
2023-03-28 14:12:01 +0200

    password generator - ONLY CONSOLE

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 
-let name = prompt("What's your name?" )
+let name = prompt("What's your name?")
 
 let surname = prompt("what's your surname?")
 
 let favouriteColor = prompt("what's your favourite color?")
 
-console.log(``)
+console.log(` 
+---------USER PROFILE---------
+
+NAME --> ${name}
+
+SURNAME --> ${surname}
+
+FAVOURITE COLOR --> ${favouriteColor}
+
+
+----------PASSWORD----------`) 
+console.log(name+surname+favouriteColor+21)
+
 
 
