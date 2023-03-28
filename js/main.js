eabdd4ad4e00479c7c25ec24139da33b6ee130ea
tpaglia493 <tommasopaglia93@outlook.it>
2023-03-28 14:52:42 +0200

    password with fraction

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ let surname = prompt("what's your surname?")
 
 let favouriteColor = prompt("what's your favourite color?")
 
+let divider = prompt("Choose any number")
+
+divider = parseInt(divider)
+
+let dividend = prompt("Choose a differrent number")
+
+dividend = parseInt(dividend)
+
+let fraction = (dividend/divider)
+
 console.log(` 
 ---------USER PROFILE---------
 
@@ -15,8 +25,8 @@ SURNAME --> ${surname}
 FAVOURITE COLOR --> ${favouriteColor}
 
 
-----------PASSWORD----------`) 
-console.log(name+surname+favouriteColor+21)
+----------PASSWORD----------`)
+console.log(name + surname + favouriteColor + fraction)
 
 
 
